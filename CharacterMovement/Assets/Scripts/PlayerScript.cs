4ca691c4f4a7245eda9519ff8049aeee0ae569e4
MarcoRodriguez935 <marcoa04.r@gmail.com>
2025-09-22 19:41:06 -0700

    Edits to movement & area

--- CharacterMovement/Assets/Scripts/PlayerScript.cs
+++ CharacterMovement/Assets/Scripts/PlayerScript.cs
@@ -5,43 +5,81 @@ public class PlayerScript : MonoBehaviour
     public CharacterController controller;
 
     float speed = 5.0f;
-    float speedRotate = 100.0f;
+    float speedRotate = 250.0f;
+    float gravity = -9.81f;
+    float verticalVelocity = 0.0f;
 
-    // Update is called once per frame
+    //float jumpHeight = 2.0f;
+    float originalHeight;
+
+    void Start() 
+    {
+        originalHeight = controller.height;
+    }
+
     void Update()
     {
-        if (Input.GetKey("w"))
+        // Player stays grounded
+        if (controller.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -1f;
+        }
+
+        // Always apply gravity
+        verticalVelocity += gravity * Time.deltaTime;
+
+        // Move forward & back
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey("w")) // Move Forward
         {
             print("w");
             Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f * Time.deltaTime * speed);
             movement = transform.TransformDirection(movement);
             controller.Move(movement);
         }
-        if (Input.GetKey("a"))
-        {
-            print("a");
-            Vector3 rotation = new Vector3(0.0f, -1.0f * Time.deltaTime * speedRotate, 0.0f);
-            transform.Rotate(rotation);
-        }
-        if (Input.GetKey("s"))
+
+        if (Input.GetKey("s")) // Move Back
         {
             print("s");
             Vector3 movement = new Vector3(0.0f, 0.0f, -1.0f * Time.deltaTime * speed);
             movement = transform.TransformDirection(movement);
             controller.Move(movement);
         }
+
+        if (Input.GetKey("w") && Input.GetKey("left shift")) // Sprint
+        {
+            print("w + shift");
+            Vector3 movement = new Vector3(0.0f, 0.0f, 1.2f * Time.deltaTime * speed);
+            movement = transform.TransformDirection(movement);
+            controller.Move(movement);
+        }
+        if (Input.GetKey("w") && Input.GetKey("c")) // Sneak (WIP)
+        {
+            print("w + c");
+            Vector3 movement = new Vector3(0.0f, 0.0f, 0.5f * Time.deltaTime * speed);
+            movement = transform.TransformDirection(movement);
+            controller.Move(movement);
+        }
+
+        // Fall from gravity
+        move.y = verticalVelocity * Time.deltaTime;
+
+        controller.Move(move);
+
+        // Rotate
+        if (Input.GetKey("a"))
+        {
+            print("a");
+            Vector3 rotation = new Vector3(0.0f, -1.0f * Time.deltaTime * speedRotate, 0.0f);
+            transform.Rotate(rotation);
+        }
+        
         if (Input.GetKey("d"))
         {
             print("d");
             Vector3 rotation = new Vector3(0.0f, 1.0f * Time.deltaTime * speedRotate, 0.0f);
             transform.Rotate(rotation);
-        }
-        if (Input.GetKey("w") & Input.GetKey("left shift")) 
-        {
-            print("w");
-            Vector3 movement = new Vector3(0.0f, 0.0f, 1.2f * Time.deltaTime * speed);
-            movement = transform.TransformDirection(movement);
-            controller.Move(movement);
         }
     }
 }
