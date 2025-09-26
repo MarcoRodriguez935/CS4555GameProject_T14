90a89a103224ed7e3bb10b687181a762385cb6c5
MarcoRodriguez935 <marcoa04.r@gmail.com>
2025-09-26 16:12:45 -0700

    Changes to Animations & Player Movement
    Animations correspond to player's movement & forward direction. Also added rigging to both the player w/ flashlight & enemies

--- CharacterMovement/Assets/Scripts/PlayerAnimation.cs
+++ CharacterMovement/Assets/Scripts/PlayerAnimation.cs
@@ -17,18 +17,64 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       bool isMoving = player.movementDirection.sqrMagnitude > 0.01f;
-       anim.SetBool("isWalking", isMoving && !player.isSprinting && !player.isSneaking);
-       anim.SetBool("isSprinting", player.isSprinting);
-       anim.SetBool("isSneaking", player.isSneaking);
+    //    bool isMoving = player.movementDirection.sqrMagnitude > 0.01f;
+    //    anim.SetBool("isWalking", isMoving && !player.isSprinting && !player.isSneaking);
+    //    anim.SetBool("isSprinting", player.isSprinting);
+    //    anim.SetBool("isSneaking", player.isSneaking);
 
-       bool movingBackwards = player.movementDirection.y < -0.1f;
-       anim.SetBool("isWalkingBackwards", movingBackwards && !player.isSprinting);
-       anim.SetBool("isJoggingBackwards", movingBackwards && player.isSprinting);
+    //    bool movingBackwards = player.movementDirection.y < -0.1f;
+    //    anim.SetBool("isWalkingBackwards", movingBackwards && !player.isSprinting);
+    //    anim.SetBool("isJoggingBackwards", movingBackwards && player.isSprinting);
 
-       if (player.interact.action.WasPressedThisFrame())
-       {
-        anim.SetTrigger("Fight");
-       }
+    //    if (player.interact.action.WasPressedThisFrame())
+    //    {
+    //     anim.SetTrigger("Fight");
+    //    }
+
+        // ------------ GPT Fix -----------------
+        // So that if the player looks backwards with the mouse, that direction becomes the new forward
+        // and S will play the walk forward animation
+
+        bool isMoving = player.movementDirection.sqrMagnitude > 0.01f;
+
+        Vector3 moveInput = new Vector3(player.movementDirection.x, 0f, player.movementDirection.y);
+        
+        Vector3 camForward = player.mainCam.transform.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
+        Vector3 camRight = player.mainCam.transform.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        Vector3 moveWorld = camForward * moveInput.z + camRight * moveInput.x;
+
+        Vector3 localMove = transform.InverseTransformDirection(moveWorld);
+
+        anim.SetBool("isWalking", isMoving && !player.isSprinting && !player.isSneaking && localMove.z > 0.1f);
+        anim.SetBool("isWalkingBackwards", isMoving && !player.isSprinting && localMove.z < -0.1f);
+        anim.SetBool("isSprinting", player.isSprinting && localMove.z > 0.1f);
+        anim.SetBool("isJoggingBackwards", player.isSprinting && localMove.z < -0.1f);
+        anim.SetBool("isSneaking", player.isSneaking);
+
+        anim.SetBool("isWalkingRight", isMoving && Mathf.Abs(localMove.x) > 0.1f && localMove.x > 0f && !player.isSprinting);
+        anim.SetBool("isWalkingLeft", isMoving && Mathf.Abs(localMove.x) > 0.1f && localMove.x < 0f && !player.isSprinting);
+
+        if (player.interact.action.WasPressedThisFrame())
+        {
+            anim.SetTrigger("Fight");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
+
+        // Roll (V key)
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            anim.SetTrigger("Roll");
+        }
+
+
     }
 }
